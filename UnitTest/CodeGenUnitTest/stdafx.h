87b401366b3cb8cf6801bf9a9f9f0ed8b0a4a04f
Michael Hopcroft <mhop@hopcroft.org>
2015-12-19 17:02:26 -0800

    Typedef __int8, __int16, __int32, and __int64 for non-Windows builds.

--- UnitTest/CodeGenUnitTest/stdafx.h
+++ UnitTest/CodeGenUnitTest/stdafx.h
@@ -14,4 +14,11 @@
 
 // TODO: reference additional headers your program requires here
 
+#else
+#include <stdint.h>
+
+typedef int8_t __int8;
+typedef int16_t __int16;
+typedef int32_t __int32;
+typedef int64_t __int64;
 #endif
