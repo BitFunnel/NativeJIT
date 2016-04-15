2075f9e587fdb578a45ceea01f9bf048490c6d66
Dan Luu <danluu@gmail.com>
2016-04-14 23:45:19 -0700

    Add licenses to cpp, asm, h, bat, and sh, files.

--- src/Assembler/asm.bat
+++ src/Assembler/asm.bat
@@ -1,3 +1,25 @@
+rem The MIT License (MIT)
+
+rem Copyright (c) 2016, Microsoft
+
+rem Permission is hereby granted, free of charge, to any person obtaining a copy
+rem of this software and associated documentation files (the "Software"), to deal
+rem in the Software without restriction, including without limitation the rights
+rem to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+rem copies of the Software, and to permit persons to whom the Software is
+rem furnished to do so, subject to the following conditions:
+
+rem The above copyright notice and this permission notice shall be included in
+rem all copies or substantial portions of the Software.
+
+rem THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+rem IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+rem FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+rem AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+rem LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+rem OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+rem THE SOFTWARE.
+
 "c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\x86_amd64\ml64.exe" /nologo /Sn /FlTestAsm.lst TestAsm.asm
 REM "c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\x86_amd64\ml64.exe" /nologo /FlTestAsm.lst /Sn TestAsm.asm
 type TestAsm.lst
