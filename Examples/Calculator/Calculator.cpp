95d4dc910475f0029acfd5e44a36398dcaf1fa1b
Dan Luu <danluu@gmail.com>
2016-05-02 18:38:30 -0700

    Make calc example more uniform.

--- Examples/Calculator/Calculator.cpp
+++ Examples/Calculator/Calculator.cpp
@@ -20,27 +20,17 @@ int64_t calc() {
     ExecutionBuffer codeAllocator(8192);
     Allocator allocator(8192);
     FunctionBuffer code(codeAllocator, 8192);
-    Function<int64_t, int64_t, int64_t> expr(allocator, code);
-
-    int numArgs = 2;
-    int curArg = 0;
+    Function<int64_t> expr(allocator, code);
 
     std::string tok;
     std::vector<int64_t> stack;
-    NativeJIT::Node<int64_t> *temp = NULL;
+    std::vector<NativeJIT::Node<int64_t>*> jitStack;
 
-    std::vector<int64_t> args;
     while (std::cin >> tok) {
         int64_t digits;
         if (std::istringstream(tok) >> digits) {
-            if (curArg < numArgs) {
-                stack.push_back(digits);
-                args.push_back(digits);
-                ++curArg;
-                std::cout << "Add token to stack" << std::endl;
-            } else {
-                std::cout << "Out of arguments. Please enter an operator.\n";
-            }
+            stack.push_back(digits);
+            jitStack.push_back(&expr.Immediate(digits));
         } else {
             if (stack.size() < 2) {
                 std::cout << "Not enough operands. Please enter an operand.\n";
@@ -51,16 +41,43 @@ int64_t calc() {
                 int64_t op2 = stack.back(); stack.pop_back();
                 int64_t op1 = stack.back(); stack.pop_back();
                 stack.push_back(op1 + op2);
-                --curArg;
-                std::cout << "Add node" << std::endl;
-                temp = &expr.Add(expr.GetP1(), expr.GetP2());
+
+
+                auto right = jitStack.back(); jitStack.pop_back();
+                auto left = jitStack.back(); jitStack.pop_back();
+                jitStack.push_back(&(expr.Add(*left, *right)));
             } else if (tok == "*") {
                 int64_t op2 = stack.back(); stack.pop_back();
                 int64_t op1 = stack.back(); stack.pop_back();
                 stack.push_back(op1 * op2);
-                --curArg;
-                std::cout << "Mul node" << std::endl;
-                temp = &expr.Mul(expr.GetP1(), expr.GetP2());
+
+                auto right = jitStack.back(); jitStack.pop_back();
+                auto left = jitStack.back(); jitStack.pop_back();
+                jitStack.push_back(&(expr.Mul(*left, *right)));
+            } else if (tok == "-") {
+                int64_t op2 = stack.back(); stack.pop_back();
+                int64_t op1 = stack.back(); stack.pop_back();
+                stack.push_back(op1 - op2);
+
+                auto right = jitStack.back(); jitStack.pop_back();
+                auto left = jitStack.back(); jitStack.pop_back();
+                jitStack.push_back(&(expr.Sub(*left, *right)));
+            } else if (tok == "|") {
+                int64_t op2 = stack.back(); stack.pop_back();
+                int64_t op1 = stack.back(); stack.pop_back();
+                stack.push_back(op1 | op2);
+
+                auto right = jitStack.back(); jitStack.pop_back();
+                auto left = jitStack.back(); jitStack.pop_back();
+                jitStack.push_back(&(expr.Or(*left, *right)));
+            } else if (tok == "&") {
+                int64_t op2 = stack.back(); stack.pop_back();
+                int64_t op1 = stack.back(); stack.pop_back();
+                stack.push_back(op1 & op2);
+
+                auto right = jitStack.back(); jitStack.pop_back();
+                auto left = jitStack.back(); jitStack.pop_back();
+                jitStack.push_back(&(expr.And(*left, *right)));
             } else {
                 std::cout << "Invalid input: " << tok << std::endl;
             }
@@ -68,8 +85,8 @@ int64_t calc() {
     }
 
     std::cout << "Compiling" << std::endl;
-    auto fn = expr.Compile(*temp);
-    int64_t res = fn(args[0], args[1]);
+    auto fn = expr.Compile(*jitStack.back());
+    auto res = fn();
     std::cout << "JIT result:normal result -- " << res << ":" << stack.back() << std::endl;
     assert(res == stack.back());
     return stack.back();
