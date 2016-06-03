5eaf4b5946ffd39d548064293f7008afb9c0219b
Dan Luu <danluu@gmail.com>
2016-06-02 20:32:11 -0700

    Remove stdafx (VC specific pre-comp headers).

--- src/NativeJIT/CallNode.cpp
+++ src/NativeJIT/CallNode.cpp
@@ -20,7 +20,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#include "stdafx.h"
 
 #include "NativeJIT/BitOperations.h"
 #include "NativeJIT/CodeGen/CallingConvention.h"
